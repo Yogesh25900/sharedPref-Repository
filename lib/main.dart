506ba6d997599bd534fd1eb3d1d0cad1fbf8bee6
yogesh25900 <chaudharyyogesh258@gmail.com>
2025-12-21 10:33:04 +0545

    add

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:shared_pref/view/shared_pref_view.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shared Preference',
+      home: SharedPrefView(),
+    ),
+  );
+}
---
+++ lib/model/user_model.dart
@@ -0,0 +1,12 @@
+// class UserModel {
+//   final String  id,
+//   final String fname,
+//   final String age,
+
+//   UserModel({
+//     required this.id,
+//     required this.fname,
+//     required this.age,
+//   });
+// };
+
